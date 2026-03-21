47472b491c9c632656ad167bda7c5d3f866fba91
Raman Khiva <ramandeep01167@gmail.com>
2026-03-21 11:26:27 +0000

    change project structure

---
+++ Turbo-Shadcn-setup/apps/web/app/login/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@workspace/ui/components/button"
+import { LoginForm } from "@workspace/ui/components/login-form"
+export default function Page() {
+  return (
+    <main>
+      <h2>this is login page</h2>
+      <Button>Login</Button>
+      <LoginForm />
+    </main>
+  )
+}
