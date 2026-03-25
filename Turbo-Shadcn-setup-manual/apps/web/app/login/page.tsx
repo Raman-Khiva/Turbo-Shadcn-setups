76851dc8b7e41bd24fe297681e66d3fbb1dad93a
Raman Khiva <ramandeep01167@gmail.com>
2026-03-25 17:41:13 +0000

    last push from joban-az-vm

---
+++ Turbo-Shadcn-setup-manual/apps/web/app/login/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@repo/ui/button";
+import { LoginForm } from "@repo/ui/components/login-form";
+export default function Page() {
+  return (
+    <main>
+      <h2>this is login page</h2>
+      <Button>Login</Button>
+      <LoginForm />
+    </main>
+  );
+}
